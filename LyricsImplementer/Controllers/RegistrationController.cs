ad6dfb010780d6994a17272b38dfdf16f38e15e6
Pavel <chpv24fev@gmail.com>
2018-10-09 23:01:57 +0300

    Some registration login was added.
    Model validation and registration views was added.

--- LyricsImplementer/Controllers/RegistrationController.cs
+++ LyricsImplementer/Controllers/RegistrationController.cs
@@ -1,17 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LyricsImplementer.Models;
 
 namespace LyricsImplementer.Controllers
 {
     public class RegistrationController : Controller
     {
-        // GET: Registration
+        private LyricsDBContext context = new LyricsDBContext();
+        private User user = new User();
+
+        [HttpGet]
         public ActionResult NewUser()
         {
             return View();
+        }
+
+        //Validate logic must be.
+        [HttpPost]
+        public ActionResult NewUser(User user)
+        {
+            this.user.Login = user.Login;
+            this.user.Password = user.Password;
+            this.user.Email = user.Email;
+            return View("UsersData");
+        }
+
+        [HttpGet]
+        public ActionResult UsersData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UsersData(User user)
+        {
+            user.Login = this.user.Login;
+            user.Password = this.user.Password;
+            user.Email = this.user.Email;
+            if (ModelState.IsValid)
+            {
+                context.Entry(user).State = System.Data.Entity.EntityState.Added;
+                context.SaveChanges();
+                return View("Success");
+            }
+            
+            return View(user);
+        }
+
+        [HttpGet]
+        public JsonResult CheckLogin(string login)
+        {
+            var result = false;
+            var users = from u in context.Users.AsNoTracking()
+                        where u.Login == login
+                        select u;
+            User user = users.ToList()[0];
+            if (user is null)
+            {
+                result = true;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult CheckNickname(string Nickname)
+        {
+            var result = false;
+            var nicknames = from n in context.Users.AsNoTracking()
+                            where n.Nickname == Nickname
+                            select n;
+            User user = nicknames.ToList()[0];
+            if (user is null)
+            {
+                result = true;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult CheckEmail(string email)
+        {
+            var result = false;
+            var emails = from e in context.Users.AsNoTracking()
+                            where e.Email == email
+                            select e;
+            User user = emails.ToList()[0];
+            if (user is null)
+            {
+                result = true;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
--- LyricsImplementer/Models/User.cs
+++ LyricsImplementer/Models/User.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace LyricsImplementer.Models
 {
     public class User
     {
+        [ScaffoldColumn(false)]
         public int Id { get; set; }
+        [Required (ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Логин")]
+        [Remote("CheckLogin", "Registration", ErrorMessage = "Login is already exist.")]
         public string Login { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Никнэйм")]
+        [Remote("CheckNickname", "Registration", ErrorMessage = "Nickname is already exist.")]
         public string Nickname { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [Display(Name = "Электронная почта")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
+        [Remote("CheckEmail", "Registration", ErrorMessage = "User with this email is already exist.")]
+        public string Email { get; set; }
 
         public string Surname { get; set; }
         public string Name { get; set; }
         public string Patronymic { get; set; }
+
+        [System.Web.Mvc.Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Display(Name = "Подтверждение пароля")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
 
         public DateTime RegistrationDate { get; set; }
     }
